856c7eb5e99449ad52368f5382a35a524abec183
Vugar Mammadli <l00pss@outlook.com>
2021-07-04 04:48:04 +0400

    It's just a start

--- NovaRindoLite/Form1.Designer.cs
+++ NovaRindoLite/Form1.Designer.cs
@@ -718,7 +718,7 @@ namespace NovaRindoLite
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Roboto Medium", 8.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label2.Location = new System.Drawing.Point(305, 7);
+            this.label2.Location = new System.Drawing.Point(312, 7);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(583, 34);
             this.label2.TabIndex = 1;
